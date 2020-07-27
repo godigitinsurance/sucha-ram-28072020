dbea5bb32c4e6db9f304f2a1b996eb75907b6e5a
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-27 12:35:22 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Sucha Ram");
+$("#policyNumber").text("ESI4704272");
+$("#startDate").text("28-Jul-2020");
+$("#endDate").text("27-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08BT0929");
+$("#make").text("BAJAJ");
+$("#model").text("PLATINA");
+$("#vehicalSubType").text("ALLOY WHEELS");
+$("#engineNo").text("DZUBTM99606");
+$("#chassis").text("MD2DDDZZZTPM44748");
